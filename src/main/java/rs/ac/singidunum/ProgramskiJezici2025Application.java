1925abe8fff0a023610c39a7b3dfa6b34ac4cd1d
Jovan Tomovic - 2023200181 <jovan.tomovic.23@singimail.rs>
2025-03-29 12:40:04 +0100

    Generated the project

---
+++ src/main/java/rs/ac/singidunum/ProgramskiJezici2025Application.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramskiJezici2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramskiJezici2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/singidunum/ProgramskiJezici2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramskiJezici2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
